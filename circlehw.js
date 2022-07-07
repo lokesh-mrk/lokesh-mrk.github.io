fcb6222cc0366be62bbfeaf3630217db1e22b985
lokesh-mrk <108858414+lokesh-mrk@users.noreply.github.com>
2022-07-07 13:35:42 +0530

    circle file
    circle file

---
+++ circlehw.js
@@ -0,0 +1,9 @@
+function find(){
+	var num1 = document.getElementById('t1').value;
+	var area = 3.14*num1*num1;
+	var perimeter = 2*3.14*num1;
+	document.getElementById('t2').value = area;
+	document.getElementById('t3').value = perimeter;
+	
+	
+}
