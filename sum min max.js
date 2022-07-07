f7fe6b99dd1d11114fd50aa172d38c3415702b48
lokesh-mrk <108858414+lokesh-mrk@users.noreply.github.com>
2022-07-07 13:45:26 +0530

    min, max, sum files
    min, max, sum files

---
+++ sum min max.js
@@ -0,0 +1,26 @@
+function sum(){
+
+var a = document.getElementById('firstname').value;
+var b = document.getElementById('lastname').value;
+var c = parseInt(a) + parseInt(b);
+
+console.log(c );
+}
+function sub(){
+
+var a = document.getElementById('firstname').value;
+var b = document.getElementById('lastname').value;
+console.log( a - b);
+}
+function mul(){
+	
+var a = document.getElementById('firstname').value;
+var b = document.getElementById('lastname').value;
+console.log(  a * b);
+}
+function div(){
+	
+var a = document.getElementById('firstname').value;
+var b = document.getElementById('lastname').value;
+console.log( a / b);
+}
