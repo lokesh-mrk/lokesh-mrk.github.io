f3a8519dea1fe28da9c17a1551cef955d3b9e3b1
lokesh-mrk <108858414+lokesh-mrk@users.noreply.github.com>
2022-07-07 19:30:02 +0530

    files
    files

--- first.js
+++ first.js
@@ -1,26 +1,29 @@
 function sum(){
 
-var a = document.getElementById('firstname').value;
-var b = document.getElementById('lastname').value;
+var a = document.getElementById('id1').value;
+var b = document.getElementById('id2').value;
 var c = parseInt(a) + parseInt(b);
 
-console.log(c );
+console.log(c);
 }
+
 function sub(){
 
-var a = document.getElementById('firstname').value;
-var b = document.getElementById('lastname').value;
-console.log( a - b);
+var a = document.getElementById('id1').value;
+var b = document.getElementById('id2').value;
+console.log(a - b);
 }
+
 function mul(){
 	
-var a = document.getElementById('firstname').value;
-var b = document.getElementById('lastname').value;
-console.log(  a * b);
+var a = document.getElementById('id1').value;
+var b = document.getElementById('id2').value;
+console.log(a * b);
 }
+
 function div(){
 	
-var a = document.getElementById('firstname').value;
-var b = document.getElementById('lastname').value;
-console.log( a / b);
+var a = document.getElementById('id1').value;
+var b = document.getElementById('id2').value;
+console.log(a / b);
 }
